8ce3b216a33503ca4bb59e45f47e8c1c0bd4409c
Dr-Sievert <thsi.privat@gmail.com>
2025-08-21 05:04:24 +0200

    Working version

--- src/main/java/net/sievert/loot_blacklist/LootBlacklist.java
+++ src/main/java/net/sievert/loot_blacklist/LootBlacklist.java
@@ -1,19 +1,46 @@
 package net.sievert.loot_blacklist;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.fabricmc.fabric.api.loot.v3.LootTableSource;
+import net.minecraft.loot.LootTable;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Main entrypoint for the Loot Blacklist mod.
+ */
 public class LootBlacklist implements ModInitializer {
 
+	/** Mod ID constant */
 	public static final String MOD_ID = "loot_blacklist";
 
+	/** Logger instance for this mod */
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	/** Global config, loaded at startup */
+	public static LootBlacklistConfig CONFIG;
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Initializing Loot Blacklist");
+		CONFIG = LootBlacklistConfig.loadOrCreate();
 
+		LootTableEvents.MODIFY.register((RegistryKey<LootTable> key,
+										 LootTable.Builder tableBuilder,
+										 LootTableSource source,
+										 RegistryWrapper.WrapperLookup registries) -> {
+			if (!CONFIG.blacklist.isEmpty()) {
+				Identifier tableId = key.getValue();
+				int removed = LootFilter.filterLootTableBuilder(tableBuilder, CONFIG.blacklist, tableId);
+
+				if (removed > 0) {
+					LOGGER.info("Applied loot blacklist: removed {} entries from {}", removed, tableId);
+				}
+			}
+		});
 	}
 }
---
+++ src/main/java/net/sievert/loot_blacklist/mixin/ItemEntryAccessor.java
@@ -0,0 +1,20 @@
+package net.sievert.loot_blacklist.mixin;
+
+import net.minecraft.item.Item;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.registry.entry.RegistryEntry;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+/**
+ * Accessor mixin for {@link ItemEntry}.
+ * Exposes the private 'item' field so blacklist code
+ * can inspect which Item a loot entry refers to.
+ */
+@Mixin(ItemEntry.class)
+public interface ItemEntryAccessor {
+
+    /** @return the registry entry of the Item this loot entry points to */
+    @Accessor("item")
+    RegistryEntry<Item> getItemEntry();
+}
