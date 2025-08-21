0c18d43a3a0a05c4a1c5a07aebd7ed8e5d398d45
Dr-Sievert <thsi.privat@gmail.com>
2025-08-21 02:26:09 +0200

    Basic repo

---
+++ src/main/java/net/sievert/loot_blacklist/LootBlacklist.java
@@ -0,0 +1,19 @@
+package net.sievert.loot_blacklist;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LootBlacklist implements ModInitializer {
+
+	public static final String MOD_ID = "loot_blacklist";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Initializing Loot Blacklist");
+
+	}
+}
